654eeb466d94dc161a768fc1a779a064bd93212b
kevinjohnkiely <kevinjohnkiely1@hotmail.com>
2020-11-18 21:23:54 +0000

    Updates

---
+++ precache-manifest.a548dbf59e5fcbd02ef0b4c4b4f1af78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40dd39db3866efaf78142f8d0bc8ee76",
+    "url": "/shopping-list-redux/index.html"
+  },
+  {
+    "revision": "8ddd1066876cf87bf928",
+    "url": "/shopping-list-redux/static/css/main.20488da1.chunk.css"
+  },
+  {
+    "revision": "3c48fda4da626c709f42",
+    "url": "/shopping-list-redux/static/js/2.9cf08f7e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopping-list-redux/static/js/2.9cf08f7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ddd1066876cf87bf928",
+    "url": "/shopping-list-redux/static/js/main.987cd036.chunk.js"
+  },
+  {
+    "revision": "f241b9c707c8a3c1a4ee",
+    "url": "/shopping-list-redux/static/js/runtime-main.cb5e3713.js"
+  }
+]);
